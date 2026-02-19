412c4b013bf6fc2e8471d61b06c0b32a9f40af16
栀染 <imbue2025@outlook.com>
2026-02-19 01:07:12 +0000

    feat: 为 Stats 添加 Replay

--- include/Common.h
+++ include/Common.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <nlohmann/json.hpp>
 
 #define SOCKET_PATH "/tmp/tcp_turbo.sock"
 #define PID_FILE "/var/run/tcp_turbo.pid"
+#define DEFAULT_RELAY_LOCAL_PORT 12000 
+
+struct TcpStats {
+    long long in_segs = 0;
+    long long out_segs = 0;
+    long long retrans_segs = 0;
+    double retrans_rate = 0.0;
+    long long relay_bytes_tx = 0;
+    long long relay_bytes_rx = 0;
+    int relay_active_conns = 0;
+};
 
 enum class CommandType {
     ENABLE,
@@ -13,13 +23,4 @@ enum class CommandType {
     GET_STATS,
     STOP_DAEMON,
     PING
-};
-
-struct TcpStats {
-    long long in_segs = 0;
-    long long out_segs = 0;
-    long long retrans_segs = 0;
-    long long in_errs = 0;
-    long long out_rsts = 0;
-    double retrans_rate = 0.0;
 };
